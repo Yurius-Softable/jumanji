796b744580e9ce794098b56921b105ea87e9bfd7
Yurius-Softable <yurius.softable@gmail.com>
2020-02-04 18:45:01 +0300

    first commit

---
+++ Assets/Scripts/UnitScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitScript : MonoBehaviour
+{
+    void Start()
+    {
+        transform.position = new Vector2(-36.7f, 32);
+    }
+    void Update()
+    {
+        transform.Translate(new Vector2(10, 0) * Time.deltaTime);
+    }
+}
